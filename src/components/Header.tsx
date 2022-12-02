c4b5127d629bd13ca7a29e59390084178f6387b2
daviramosds <davirds.dev@gmail.com>
2022-12-02 18:00:41 -0300

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import Logo from '../assets/logo.svg'
+import SearchIcon from '../assets/icon-search.svg'
+import BagIcon from '../assets/icon-bag.svg'
+
 export function Header() {
   return (
-    <header>
-      <ul>
-        <li>
-          <img />
+    <header className='flex items-center justify-center'>
+      <ul className='flex items-center justify-between w-full px-16 py-8 font-bold text-lg'>
+        <li className='w-11'>
+          <img src={Logo} draggable={false}  />
         </li>
 
         <li>
@@ -22,15 +26,15 @@ export function Header() {
           <a href="">CUSTOMIZAR</a>
         </li>
 
-        <li>
-          <div>
-            <img />
-            <input type={"search"}></input>
+        <li >
+          <div className='bg-white flex h-9'>
+            <img src={SearchIcon} className="w-6 mx-2" />
+            <input type={"search"} className="w-40 outline-none font-normal text-sm" placeholder='Pesquisar'></input>
           </div>
         </li>
 
-        <li>
-          <img />
+        <li className='w-11'>
+          <img src={BagIcon} />
         </li>
       </ul>
     </header>
