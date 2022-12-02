a4ddb65e21d50b67968be96ec22135fa5c6d5eca
daviramosds <davirds.dev@gmail.com>
2022-12-02 17:45:42 -0300

    starting project + tailwindcss + starting header

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+export function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <img />
+        </li>
+
+        <li>
+          <a href="">HOMEM</a>
+        </li>
+
+        <li>
+          <a href="">MULHER</a>
+        </li>
+
+        <li>
+          <a href="">CRIANÇA</a>
+        </li>
+
+        <li>
+          <a href="">CUSTOMIZAR</a>
+        </li>
+
+        <li>
+          <div>
+            <img />
+            <input type={"search"}></input>
+          </div>
+        </li>
+
+        <li>
+          <img />
+        </li>
+      </ul>
+    </header>
+  );
+}
